ceb5c64ccee39395504fd8b04e04e971ffca646a
tmlewin <dexterity4u@gmail.com>
2021-12-29 17:15:48 -0600

    "init"

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () =>{
+  return (
+    <>
+      <Header />
+      <main>
+      <h1>Welcome to ProShop</h1>
+      </main>
+      <Footer />
+      
+    </>
+  );
+}
+
+export default App;
